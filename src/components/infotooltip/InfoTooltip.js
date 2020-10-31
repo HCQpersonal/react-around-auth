927ceec82022de04fe49662d9edf06ee997adff2
Jessica Perelman <hotcheetoqueen@users.noreply.github.com>
2020-10-31 11:15:23 -0700

    hotfix/pull feedback modal over general modal with form component

--- src/components/infotooltip/InfoTooltip.js
+++ src/components/infotooltip/InfoTooltip.js
@@ -6,7 +6,7 @@ import '../../blocks/modal/__background/_tooltip/modal__background_tooltip.css';
 
 function InfoTooltip(props) {
     return (
-        <PopupWithForm className='modal__background modal__background_tooltip' name='tooltip' isOpen={props.isOpen} onClose={props.onClose}>
+        <PopupWithForm className='modal__background modal__background_tooltip modal_tooltip' name='tooltip' isOpen={props.isOpen} onClose={props.onClose}>
             <img src={props.feedback === 'success' ? successIcon : failIcon} alt='success or failure feedback icon' />
             <h2 className='modal__title modal__title_tooltip'>{props.feedback ==='success' ? 'Success! You have now been registered.' : 'Oops, something went wrong! Please try again.'}</h2>
         </PopupWithForm>
