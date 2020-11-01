397e8663c0066633411caaa15332525f48066c9f
Jessica Perelman <hotcheetoqueen@users.noreply.github.com>
2020-11-01 09:35:58 -0800

    Updates

---
+++ precache-manifest.4a489b813471de91165d1b2c3c88643e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c06b708184f5d7c0d6a750e64d1896e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d16dc7cf607e70388d5",
+    "url": "/static/css/main.4fbef1fb.chunk.css"
+  },
+  {
+    "revision": "d46de639b5d3675c65b7",
+    "url": "/static/js/2.c5b6ff2a.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/static/js/2.c5b6ff2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d16dc7cf607e70388d5",
+    "url": "/static/js/main.02f2a753.chunk.js"
+  },
+  {
+    "revision": "137973cebaa227192527",
+    "url": "/static/js/runtime-main.b53ce7bb.js"
+  },
+  {
+    "revision": "5bbb05f725aa334a73acf3174e255f4e",
+    "url": "/static/media/Filled_heart.5bbb05f7.svg"
+  },
+  {
+    "revision": "f1eadf003e031c9f8f9cd30f310bbad2",
+    "url": "/static/media/Grand_Canyon.f1eadf00.jpg"
+  },
+  {
+    "revision": "e37354838bf8078ac0d296217613533f",
+    "url": "/static/media/Inter-Black.e3735483.woff"
+  },
+  {
+    "revision": "79260e5b693fac2ee373b659f01a2bdf",
+    "url": "/static/media/Inter-Bold.79260e5b.woff"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "/static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "2c852ac22bfbc986bd0cf0d5b086176e",
+    "url": "/static/media/Joshua_Tree.2c852ac2.jpg"
+  },
+  {
+    "revision": "216c3644ed56fe92d5619bdeb50a8f6a",
+    "url": "/static/media/New_Orleans.216c3644.jpg"
+  },
+  {
+    "revision": "9d4361a4febb98320ff319b9ea575902",
+    "url": "/static/media/Seattle.9d4361a4.jpg"
+  },
+  {
+    "revision": "a77c7ec1dbad99bd57317a8308c6736f",
+    "url": "/static/media/Seqouia.a77c7ec1.jpg"
+  },
+  {
+    "revision": "8a68246e1d4c7c6375be648664b2d252",
+    "url": "/static/media/White_Sands.8a68246e.jpg"
+  }
+]);
